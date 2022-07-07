91df6bc846e28a03bdd336bf79b7cce9d685c1ba
DuongLoi17091997 <97817325+DuongLoi17091997@users.noreply.github.com>
2022-07-07 14:02:37 +0700

    back up 772022

---
+++ src/acounts/Account.java
@@ -0,0 +1,23 @@
+package acounts;
+
+public class Account {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public class Accounts {
+		String lastName;
+		String firstName;
+		String fullName;
+		String password;
+		String gender;
+		String dateOfBirth;
+	}
+	public class Users extends Accounts {
+		
+	}
+	public class Admin extends Accounts {
+		
+	}
+}
---
+++ src/features/GroupChat.java
@@ -0,0 +1,18 @@
+package features;
+
+public class GroupChat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public class GrChat {
+		
+	}
+	public class PublicGr extends GrChat {
+		
+	}
+	public class PrivateGr extends GrChat {
+		
+	}
+}
